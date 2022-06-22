b54d9034020a4220a7eb8c3134ef3035f1e6ed05
Chandler Davis <echandlerdavis@gmail.com>
2022-06-22 15:21:06 -0700

    initial commit, schema.sql

---
+++ server.js
@@ -0,0 +1,9 @@
+//okay so it looks like this is going to be some sort of inquirer app situation, /n
+// but instead of inputing the information into a web page it's going to be put into 
+// a database. 
+
+
+// so, refer to both the read_me generator shit and the api/ routing things. 
+
+//also refer to this week's solved mini projects to do the app.put situation. 
+//steal the package.json stuff from other assignments. 
